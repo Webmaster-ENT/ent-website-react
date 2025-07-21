389c95e45f22a6395bda2d66d4180653211e7546
Pipip-zen <rafif.nuha@gmail.com>
2025-07-21 15:02:09 +0700

    feat(member section): update responsive

--- src/components/partials/member/MemberCard.tsx
+++ src/components/partials/member/MemberCard.tsx
@@ -17,7 +17,7 @@ const MemberCard: React.FC<MemberCardProps> = ({ generation, name, role, major, 
 
   return (
     <div className={`relative transition-transform duration-500 ${isActive ? 'scale-105' : 'scale-90'}`}>
-      <div className="w-112 h-75 bg-yellow-200 rounded-3xl p-6 flex items-end relative">
+      <div className="w-90 h-75 bg-yellow-200 rounded-3xl p-6 flex items-end relative">
         <div className="w-full h-62 bg-white rounded-2xl shadow-lg relative flex"> {/* Adjusted height to h-72 */}
           <div className="w-1/2 p-4 flex flex-col justify-center">
             <h3 className="text-4xl font-bold">{name}</h3>
@@ -28,12 +28,12 @@ const MemberCard: React.FC<MemberCardProps> = ({ generation, name, role, major, 
               <p>{majorLine3}</p>
             </div>
           </div>
-          <div className="absolute -right-6 -top-12"> {/* Adjusted positioning */}
-            <img src={image} alt={name} className="w-60 h-80 object-cover" /> {/* Adjusted image size */}
+          <div className="absolute -right-6 -bottom-6"> {/* Adjusted positioning */}
+            <img src={image} alt={name} className="w-50 h-70 object-cover rounded-3xl" /> {/* Adjusted image size */}
           </div>
         </div>
       </div>
-      <div className="absolute top-1 left-6">
+      <div className="absolute top-1 left-9">
         <span className="text-yellow-600 font-semibold">{generation}</span>
       </div>
     </div>
