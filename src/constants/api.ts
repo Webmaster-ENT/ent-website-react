9435b0c2bc66e2576231408ab2b6fe4072b3b2be
Yaser Syafa <syafayaser@gmail.com>
2025-07-28 00:32:51 +0700

    feat: add axios instance

---
+++ src/constants/api.ts
@@ -0,0 +1,9 @@
+/* API Endpoints */
+export const API_ENDPOINTS = {} as const;
+
+/* API Configuration */
+export const API_CONFIG = {
+  BASE_URL: "http://localhost:8000/api/v1",
+  TIMEOUT: 10_000,
+  RETRY_ATTEMPTS: 3,
+};
