79749d1fba10255bdcbfea96df9f240233053513
Pipip-zen <rafif.nuha@gmail.com>
2025-08-04 21:01:32 +0700

    feat(member): change color card

--- src/components/partials/member/MemberCard.tsx
+++ src/components/partials/member/MemberCard.tsx
@@ -6,10 +6,10 @@ interface MemberCardProps {
   major: string;
   image: string;
   isActive: boolean;
-  divisionColor?: 'green' | 'blue' | 'pink' | 'yellow' | 'purple';
+  divisionColor?: 'green' | 'blue' | 'pink' | 'yellow' | 'purple' | 'red' | 'orange' | 'cyan';
 }
 
-const MemberCard = ({ generation, name, role, major, image, isActive, divisionColor = 'yellow' }: MemberCardProps) => {
+const MemberCard = ({ generation, name, role, major, image, isActive, divisionColor }: MemberCardProps) => {
   const majorWords = major.split(' ');
   const majorLine1 = majorWords[0];
   const majorLine3 = majorWords.slice(-2).join(' ');
@@ -21,12 +21,27 @@ const MemberCard = ({ generation, name, role, major, image, isActive, divisionCo
     blue: 'bg-blue-200',
     pink: 'bg-pink-200',
     yellow: 'bg-yellow-200',
-    purple: 'bg-purple-200'
+    purple: 'bg-purple-200',
+    red: 'bg-red-200',
+    orange: 'bg-orange-200',
+    cyan: 'bg-cyan-200'
   };
+
+  const textColorClasses = {
+  green: 'text-green-600',
+  blue: 'text-blue-600',
+  pink: 'text-pink-600',
+  yellow: 'text-yellow-600',
+  purple: 'text-purple-600',
+  red: 'text-red-600',
+  orange: 'text-orange-600',
+  cyan: 'text-cyan-600'
+};
+
 
   return (
     <div className={`relative transition-transform duration-500 ${isActive ? 'scale-105' : 'scale-90'}`}>
-      <div className={`w-90 h-75 ${colorClasses[divisionColor]} rounded-3xl p-6 flex items-end relative`}>
+      <div className={`w-90 h-75 ${divisionColor ? colorClasses[divisionColor] : ''}  rounded-3xl p-6 flex items-end relative`}>
         <div className="w-full h-62 bg-white rounded-2xl shadow-lg relative flex"> {/* Adjusted height to h-72 */}
           <div className="w-1/2 p-4 flex flex-col justify-center">
             <h3 className="text-4xl font-bold">{name}</h3>
@@ -43,13 +58,9 @@ const MemberCard = ({ generation, name, role, major, image, isActive, divisionCo
         </div>
       </div>
       <div className="absolute top-1 left-9">
-        <span className={`font-semibold ${
-          divisionColor === 'green' ? 'text-green-600' :
-          divisionColor === 'blue' ? 'text-blue-600' :
-          divisionColor === 'pink' ? 'text-pink-600' :
-          divisionColor === 'yellow' ? 'text-yellow-600' :
-          'text-purple-600'
-        }`}>{generation}</span>
+        <span className={`font-semibold ${divisionColor ? textColorClasses[divisionColor] : ''}`}>
+          {generation}
+        </span>
       </div>
     </div>
   );
